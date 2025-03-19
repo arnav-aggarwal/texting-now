749f315e80eaf47bdb456efc7f8132c6caf7b663
Arnav Aggarwal <arnavaggrwl@gmail.com>
2025-03-19 13:23:06 -0600

    Audio recording and playback works

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AudioRecorder from '../components/AudioRecorder';
+
+function LandingPage() {
+  return (
+    <div>
+      <AudioRecorder />
+    </div>
+  )
+}
+
+export default LandingPage;
